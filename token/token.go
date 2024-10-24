58ef37f8c825ca9020e5c8d4061f1760bf3a8084
maxingfang <maxingfang@meituan.com>
2024-10-24 20:02:30 +0800

    feat: init lexer
    - 增加对双目运算符、函数等的支持

--- token/token.go
+++ token/token.go
@@ -26,6 +26,9 @@ const (
 	LT = "<"
 	GT = ">"
 
+	EQ     = "=="
+	NOT_EQ = "!="
+
 	// 分隔符
 	COMMA     = ","
 	SEMICOLON = ";"
@@ -38,11 +41,21 @@ const (
 	// 关键字
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
