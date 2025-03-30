745920d44e3c446921c65c48a22880ccd37486f4
maxingfang <maxingfang@meituan.com>
2025-03-30 15:20:00 +0800

    feat: [3.9] 绑定与环境

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"monkey/evaluator"
 	"monkey/lexer"
+	"monkey/object"
 	"monkey/parser"
 )
 
@@ -13,6 +14,7 @@ const PROMPT = ">>"
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 	for {
 		_, _ = fmt.Fprintf(out, PROMPT)
 		scanned := scanner.Scan()
@@ -33,7 +35,7 @@ func Start(in io.Reader, out io.Writer) {
 		io.WriteString(out, program.String())
 		io.WriteString(out, "\n")
 
-		evaluted := evaluator.Eval(program)
+		evaluted := evaluator.Eval(program, env)
 		if evaluted != nil {
 			io.WriteString(out, evaluted.Inspect())
 			io.WriteString(out, "\n")
