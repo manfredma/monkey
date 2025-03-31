ccd351d0b48906a3e6e82ea2817990b272cc24d0
maxingfang <maxingfang@meituan.com>
2025-03-31 13:50:30 +0800

    feat: [4.2.1] 词法分析器支持字符串

--- token/token.go
+++ token/token.go
@@ -12,9 +12,9 @@ const (
 	EOF     = "EOF"
 
 	// 标识符+字面量
-	IDENT = "IDENT" // add, foobar, x, y, ...
-	INT   = "INT"   // 1343456
-
+	IDENT  = "IDENT"  // add, foobar, x, y, ...
+	INT    = "INT"    // 1343456
+	STRING = "STRING" // 字符串
 	// 运算符
 	ASSIGN   = "="
 	PLUS     = "+"
